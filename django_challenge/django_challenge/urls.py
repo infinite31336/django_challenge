611d607718d2be5603a2a00a55fd2ae3f8de0684
Tony <infinite31336@gmail.com>
2025-04-30 16:48:58 +0800

    Update project name and create new app

--- django_challenge/django_challenge/urls.py
+++ django_challenge/django_challenge/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for django_blog project.
+URL configuration for django_challenge project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
