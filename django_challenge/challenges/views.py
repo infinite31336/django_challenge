95ad65653da75daa9f12a592ecec837c167ff6dc
Tony <infinite31336@gmail.com>
2025-05-02 14:16:19 +0800

    Add url patterns for app

--- django_challenge/challenges/views.py
+++ django_challenge/challenges/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
 
-# Create your views here.
+def monthly_challenge(request, month):
+    message = ''
+    if month == 'january':
+      message = 'Eat no meat for the entire month!'
+    elif month == 'february':
+      message = 'Walk at least 20 minutes every day!'
+    elif month == 'march':
+      message = 'Learn Django for at least 20 minutes every day!'
+    else:
+       return HttpResponseNotFound('This month is not supported!')
+    return HttpResponse(message)
