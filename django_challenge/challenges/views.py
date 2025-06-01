0d0ca69d147fc014a463a7d0a70c610840e3f5c6
Tony <infinite31336@gmail.com>
2025-06-01 14:33:35 +0800

    Add css and html

--- django_challenge/challenges/views.py
+++ django_challenge/challenges/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import Http404, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
 from django.template.loader import render_to_string
 
@@ -37,11 +37,11 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
   try:
     message = monthly_challenges[month]
-    response_data = render(request, 'challenges/challenge.html', {
+    return render(request, 'challenges/challenge.html', {
       'month_name': month.capitalize(),
       'text': message
     })
-    return HttpResponse(response_data)
   except:
-    return HttpResponseNotFound('<h1>This month is not supported!<h1>')
+    response_data = render_to_string('404.html')
+    raise Http404()
   
